98a4c2b474d9239f0cea17acd66e7ff6da2129ab
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-09-16 20:38:55 +0900

    update messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
   def index
+    Message.all
+  end 
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image)
   end
 end
+
