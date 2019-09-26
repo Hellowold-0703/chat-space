4e208314e809f15230c9c69726d1241d75111502
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-09-27 00:54:53 +0900

    update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,10 +2,10 @@ $(function() {
 
   var user_list = $("#user-search-result");
 
-  function appendUser(user) {
+  function appendUsers(user) {
      var html = `<div class="chat-group-user clearfix">
                    <p class="chat-group-user__name">${user.name}</p>
-                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
                  </div>`
     user_list.append(html);
   }
@@ -15,6 +15,17 @@ $(function() {
                   <p class="chat-group-user__name">${msg}</p>
                 </div>`
     user_list.append(html);
+  }
+   
+  var group_users_list = $(".js-add-user");
+
+  function appendGroupUsers(id, name) {
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    group_users_list.append(html);
   }
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -30,12 +41,22 @@ $(function() {
       $("#user-search-result").empty();
       if (users.length !== 0) {
       users.forEach(function(user) {
-        appendUser(user);
+        appendUsers(user);
       });
       }
       else {
         appendErrMsgToHTML("一致するユーザーはいません");
       }
+      $(".clearfix").on("click", ".chat-group-user__btn--add",function() {
+        $(this).parent().remove();
+        var id = $(this).attr("data-user-id");
+        console.log(id);
+        var name = $(this).attr("data-user-name");
+        appendGroupUsers(id, name);
+      });
+      $(".js-add-user").on("click", ".chat-group-user__btn", function() {
+        $(this).parent().remove();
+      });
     })
     .fail(function(){
       alert("ユーザー検索に失敗しました");
