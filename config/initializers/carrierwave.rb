65d562a066f44575501269a597b0f75657acef5e
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-10-03 01:38:12 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'ryohei-chatspace'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ryohei-chatspace
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ryohei-chatspace'
 end
